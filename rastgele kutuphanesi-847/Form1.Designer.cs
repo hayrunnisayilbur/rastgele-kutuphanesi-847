146ace836543c2e8fa42bda2235960b239ec88e2
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-24 14:23:44 +0300

    Add files via upload

---
+++ rastgele kutuphanesi-847/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace rastgele_kutuphanesi_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.lbNotlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(84, 97);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(75, 23);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // lbNotlar
+            // 
+            this.lbNotlar.FormattingEnabled = true;
+            this.lbNotlar.Location = new System.Drawing.Point(185, 97);
+            this.lbNotlar.Name = "lbNotlar";
+            this.lbNotlar.Size = new System.Drawing.Size(120, 225);
+            this.lbNotlar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(421, 373);
+            this.Controls.Add(this.lbNotlar);
+            this.Controls.Add(this.btnUret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.ListBox lbNotlar;
+    }
+}
+
---
+++ rastgele kutuphanesi-847/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rastgele_kutuphanesi_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            lbNotlar.Items.Clear();
+            Random rnd = new Random();
+
+            for( int i = 0; i < 15; i++)
+            {
+                
+                int puan = rnd.Next(0,100);
+                lbNotlar.Items.Add(puan);
+            }
+        }
+    }
+}
